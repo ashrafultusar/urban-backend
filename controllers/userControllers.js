86bda1cb789c4e47e68cb2bf2e2ae1d2956f16e8
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-02 05:17:05 +0600

    added up

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,7 +6,7 @@ const loginUser = async (req,res) => {
 
 // route for user register
 const registerUser = async (req,res) => {
-    
+    res.json({msg: "Register API Working"})
 }
 
 // route for admin login
