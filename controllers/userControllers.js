455d4be52da99f2fcbe7f53411318073252e36b8
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-02 05:11:38 +0600

    added update

---
+++ controllers/userControllers.js
@@ -0,0 +1,17 @@
+
+// route for user login
+const loginUser = async (req,res) => {
+    
+}
+
+// route for user register
+const registerUser = async (req,res) => {
+    
+}
+
+// route for admin login
+const adminLogin = async (req, res) => {
+    
+}
+
+export{loginUser,registerUser,adminLogin}
